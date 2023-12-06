5af76524e139d950d65a77be1952696bc978491b
cmartingu <152396741+cmartingu@users.noreply.github.com>
2023-12-06 13:20:26 +0100

    Update push_swap.c

--- push_swap.c
+++ push_swap.c
@@ -12,28 +12,6 @@
 
 #include "push_swap.h"
 
-void	error_list(int error)
-{
-	if (error == 0)
-		write(1, "Error, invalid arguments", 24);
-	else if (error == 1)
-		write(1, "Error, no arguments entered", 27);
-	else if (error == 2)
-		write(1, "Error, repeated arguments", 25);
-}
-
-void	free_struct(t_list *list)
-{
-	t_list *aux;
-
-	while (list)
-	{
-		aux = list->next;
-		free(list);
-		list = aux;
-	}
-}
-
 t_list	*ft_lstnew(int content)
 {
 	t_list	*sol;
@@ -61,62 +39,10 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	}
 }
 
-int	has_repeat(t_list *list)//Comprobar de uno en uno si la lista tiene números repetidos
+void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	t_list	*aux;
-
-	while (list->next)// Comparar list->content con todos los aux hasta llegar al final y liego pasar a list->next y asi lo mismo
-	{
-		aux = list->next;
-		while (aux)
-		{
-			if (list->content == aux->content)
-				return (-1);
-			aux = aux->next;
-		}
-		list = list->next;
-	}
-	return (1);
-}
-
-t_list *parse_args(int argc, char *argv[])
-{
-	t_list	*list;
-	t_list	*aux;
-	int		num;
-	int		i;
-	int		j;
-
-	i = 1;
-	list = ft_lstnew(0);
-	while (argv[i])
-	{
-		j = 0;
-		while (*(argv[i] + j))
-		{
-			if (*(argv[i] + j) >= '0' && *(argv[i] + j) <= '9')
-			{
-				num = 0 + (int){(*(argv[i] + j)) - '0'};
-				j++;
-				while (*(argv[i] + j) && *(argv[i] + j) >= '0' && *(argv[i] + j) <= '9')
-				{
-					num = (num * 10) + (int){(*(argv[i] + j)) - '0'};
-					j++;
-				}
-				ft_lstadd_back(&list, ft_lstnew(num));
-			}
-			else if (*(argv[i] + j) == ' ')
-				j++;
-			else
-				return (free_struct(list),error_list(0), NULL);
-		}
-		i++;
-	}
-	aux = list->next;
-	free(list);
-	if (has_repeat(aux) == -1)
-		return (error_list(2), free_struct(aux), NULL);
-	return (aux);
+	new->next = *lst;
+	*lst = new;
 }
 
 int	main(int argc, char *argv[])
@@ -124,10 +50,19 @@ int	main(int argc, char *argv[])
 	int 	i;
 	t_list	*list_num;
 	t_list	*aux;
+	t_list	*stackB;
 
 	if (argc <= 1)
 		return (error_list(1), 0);
 	list_num = parse_args(argc, argv);
+	stackB = ft_lstnew(4);
+	ft_lstadd_back(&stackB, ft_lstnew(5));
+	ft_lstadd_back(&stackB, ft_lstnew(6));
+	//ft_lstadd_back(&stackB, ft_lstnew(8));
+	//ft_lstadd_back(&stackB, ft_lstnew(9));
+	//ft_lstadd_back(&stackB, ft_lstnew(10));
+	pa(&list_num, &stackB);
+	printf("Stack A:\n");
 	while (list_num)
 	{
 		printf("%d\n", list_num->content);
@@ -139,6 +74,19 @@ int	main(int argc, char *argv[])
 		}
 		else
 			list_num = NULL;
+	}
+	printf("\nStack B:\n");
+	while (stackB)
+	{
+		printf("%d\n", stackB->content);
+		if (stackB->next)
+		{
+			aux = stackB->next;
+			free(stackB);
+			stackB = aux;
+		}
+		else
+			stackB = NULL;
 	}
 	return (0);
 }
