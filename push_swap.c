bc22a28edea67ce87d82f51d51c3aeacfa5ef34f
cmartingu <152396741+cmartingu@users.noreply.github.com>
2023-12-16 18:36:27 +0100

    Update push_swap.c

--- push_swap.c
+++ push_swap.c
@@ -12,89 +12,73 @@
 
 #include "push_swap.h"
 
-t_list	*ft_lstlast(t_list *lst)
+t_list	*save_args(char *argv[], int *i, t_list *list)
 {
-	t_list	*sol;
+	int	j;
 
-	sol = lst;
-	if (sol == NULL)
-		return (NULL);
-	while (sol->next != NULL)
-		sol = sol->next;
-	return (sol);
-}
-
-t_list	*ft_lstnew(int nb, int position)
-{
-	t_list	*sol;
-
-	sol = malloc(sizeof(t_list));
-	if (!sol)
-		return (NULL);
-	sol->num = nb;
-	sol->ideal_pos = position;
-	sol->next = NULL;
-	return (sol);
-}
-
-int	ft_lstsize(t_list *lst)
-{
-	int	cont;
-
-	cont = 0;
-	while (lst)
+	j = 0;
+	while (*(argv[*i] + j))
 	{
-		cont++;
-		lst = lst->next;
+		if (*(argv[*i] + j) == '-' && (*(argv[*i] + j + 1) <= '0' || \
+			*(argv[*i] + j + 1) >= '9'))
+			return (free_struct(list), write (2, "Error\n", 6), NULL);
+		if ((*(argv[*i] + j) >= '0' && *(argv[*i] + j) <= '9') || \
+			*(argv[*i] + j) == '-')
+		{
+			list = new_num(argv, &*i, &j, &list);
+			if (!list)
+				return (NULL);
+		}
+		else if (*(argv[*i] + j) == ' ')
+			j++;
+		else
+			return (free_struct(list), write (2, "Error\n", 6), NULL);
 	}
-	return (cont);
+	return (list);
 }
 
-void	ft_lstadd_back(t_list **lst, t_list *new)
+t_list	*parse_args(int argc, char *argv[])
 {
+	t_list	*list;
 	t_list	*aux;
+	int		i;
 
-	if (!*lst)
-		*lst = new;
-	else
+	i = 1;
+	argc = 0;
+	list = ft_lstnew(0, 0);
+	while (argv[i])
 	{
-		aux = *lst;
-		while (aux->next != NULL)
-			aux = aux->next;
-		aux->next = new;
+		if (argv[i][0] == '\0')
+			return (write (2, "Error\n", 6), free_struct(list), NULL);
+		list = save_args(argv, &i, list);
+		if (!list)
+			return (NULL);
+		i++;
 	}
-}
-
-void	ft_lstadd_front(t_list **lst, t_list *new)
-{
-	new->next = *lst;
-	*lst = new;
+	aux = list->next;
+	free(list);
+	if (has_repeat(aux) == -1)
+		return (write (2, "Error\n", 6), free_struct(aux), NULL);
+	return (aux);
 }
 
 int	main(int argc, char *argv[])
 {
-	t_list	*stackA;
-	//t_list	*aux;
+	t_list	*stacka;
 
 	if (argc <= 1)
-		return (error_list(1), 0);
-	stackA = parse_args(argc, argv);
-	if (check(stackA, NULL) == 1)
 		return (0);
-	pick_positions(&stackA);
-	try_check(&stackA);
-	/*printf("\n\nStack A:\n");
-	while (stackA)
-	{
-		printf("%d\n", stackA->num);
-		if (stackA->next)
-		{
-			aux = stackA->next;
-			free(stackA);
-			stackA = aux;
-		}
-		else
-			stackA = NULL;
-	}*/
+	if (argv[1][0] == '\0')
+		return (write (2, "Error\n", 6), 0);
+	stacka = parse_args(argc, argv);
+	if (!stacka)
+		return (0);
+	if (check(stacka, NULL) == 1)
+		return (0);
+	if (ft_lstsize(stacka) == 2)
+		return (ra(&stacka), 0);
+	pick_positions(&stacka);
+	principal(&stacka);
+	free_struct(stacka);
 	return (0);
 }
